20bad2be90d942051de7237cea313a2da3e6519d
pauline-wanjiku <paulinendungushiko96@gmail.com>
2023-02-14 10:10:52 +0300

    Feature:Injected the md content to the new template

--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
 import fs from "fs";
 import matter from "gray-matter";
 import { marked } from "marked";
+import path from "path";
+import { title } from "process";
 
 //read markdown file
 const readFile = (fileName) => {
   try {
     const readFile = fs.readFileSync(fileName, "utf8");
-
-    const { content, data } = matter(readFile);
-    const htmlMarked = marked(content, data.title);
-    console.log(htmlMarked);
-    // console.log({ content: content, data: data.title });
-    // return { ...content, data };
+    const parsed = matter(readFile);
+    // console.log(parsed);
+    const htmlMarked = marked(parsed.content);
+    // console.log(htmlMarked);
+    return { ...parsed, htmlMarked };
   } catch (error) {
     console.log(error);
   }
 };
-// readFile("./markDownPages/homePage.md");
+const fileName = path.join(path.resolve(), "./markDownPages/homePage.md");
+const processedFile = readFile(fileName);
+const myTemplate = fs.readFileSync(
+  path.join(path.resolve(), "./templates/index.html"),
+  "utf8"
+);
+//description, quote, date, author
 
 //Read Contents on Template file
+const templatize = (
+  template,
+  { content, title, description, quote, date, author }
+) =>
+  template
+    .replace(/<!--CONTENT-->/g, content)
+    .replace(/<!--TITLE-->/g, title)
+    .replace(/<!--DESCRIPTION-->/g, description)
+    .replace(/<!--QUOTE-->/g, quote)
+    .replace(/<!--DATE-->/g, date)
+    .replace(/<!--AUTHOR-->/g, author);
+
+console.log(templatize);
+const updatedTemplate = templatize(myTemplate, {
+  content: processedFile.content,
+  title: processedFile.data.title,
+  description: processedFile.data.description,
+  quote: processedFile.data.quote,
+  date: processedFile.data.date,
+  author: processedFile.data.author,
+});
+console.log(updatedTemplate);
